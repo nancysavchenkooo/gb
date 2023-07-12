a1bdb59de35ac77c70d3e486f8c76fc381711dd3
nancysavchenkooo <83898268+nancysavchenkooo@users.noreply.github.com>
2023-07-12 21:57:03 +0700

    Add files via upload

--- fbkun.py
+++ fbkun.py
@@ -22,9 +22,11 @@ def selenas(empas):
   }
   headers = {
     'authority': 'b-api.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
     'authorization': 'OAuth 200424423651082|2a9918c6bcd75b94cefcbb5635c6ad16',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko) [FBAN/MessengerLite;FBAV/115.0.0.2.114;FBPN/com.facebook.mlite;FBLC/ar_EG;FBBV/257412622;FBCR/Orange - STAY SAFE;FBMF/Xiaomi;FBBD/xiaomi;FBDV/Redmi 7;FBSV/9;FBCA/arm64-v8a:null;FBDM/{density=2.0,width=720,height=1369};]'
+    'user-agent': 'Dalvik/2.1.0 (Linux; U; Android 9; Redmi 7 MIUI/V11.0.6.0.PFLMIXM) [FBAN/MessengerLite;FBAV/115.0.0.2.114;FBPN/com.facebook.mlite;FBLC/ar_EG;FBBV/257412622;FBCR/Orange - STAY SAFE;FBMF/Xiaomi;FBBD/xiaomi;FBDV/Redmi 7;FBSV/9;FBCA/arm64-v8a:null;FBDM/{density=2.0,width=720,height=1369};]'
     
   }
   
@@ -39,7 +41,7 @@ def selenas(empas):
   else:
     print(empas + ' - ' + response.json()['error_msg'])
     
-with concurrent.futures.ThreadPoolExecutor(max_workers=100) as sh:
+with concurrent.futures.ThreadPoolExecutor(max_workers=1) as sh:
     e = open('sok.txt').read()
     
     for i, ee in enumerate(e.splitlines()):
